15898eb1b7febc0208eb51fc5bee21100b1f901c
David Cha <littlesadtea@gmail.com>
2020-08-28 21:09:42 -0400

    still testing responsive

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,50 @@
 import React from 'react'
 
-import NavLinks from './navLinks'
-import Footer from './footer'
-import layoutStyles from './layout.module.scss'
+// import NavLinks from './navLinks'
+// import Footer from './footer'
+// import layoutStyles from './layout.module.scss'
+import layoutStyles2 from './layout2.module.scss'
 
 const Layout = ( { children }) => {
     return (
-        <div  className={layoutStyles.mainContainer} >
-        {/* change this to main container */}
-            <div className={layoutStyles.sideBar} >
-                <NavLinks />
+        // <div  className={layoutStyles.mainContainer} >
+        // {/* change this to main container */}
+        //     <div className={layoutStyles.sideBar} >
+        //         <NavLinks />
+        //     </div>
+        //     {/* sidebar container */}
+        //         <div className={layoutStyles.mainContent} >
+        //             {children}
+        //             {/* main content container */}
+        //         </div>
+        //             <div className={layoutStyles.footer} >
+        //                 <Footer />
+        //             </div>
+        // </div>
+        // this is the previous layout with flexbox
+
+        <div className={layoutStyles2.body} >
+            <div className={layoutStyles2.mainContent} >
+                <h1>
+                    Main Container will hold main content
+                </h1>
             </div>
-            {/* sidebar container */}
-                <div className={layoutStyles.mainContent} >
-                    {children}
-                    {/* main content container */}
-                    <div className={layoutStyles.footer} >
-                        <Footer />
-                    </div>
+            <div className={layoutStyles2.sideBar} >
+                <p>Side bar</p> 
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Content</li>
+                    <li>Contact</li>
+                </ul>
+                <div>
+                    Footer
                 </div>
+            </div>
         </div>
+        // this part will contain a responsive sidebar and a hidden button
     )
+
 }
 // this file shows the structure of the component tree, it is imported to every page and the children is the data within the pages. 
 // In every other page component, it'll be wrapped by this layout component to make it more attractive and organized 
