d4df5d4583dc587d38a4b018df27f6bee8edda50
David Cha <littlesadtea@gmail.com>
2020-09-02 21:29:22 -0400

    added custom 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function FourZeroFour() {
+  return (
+    <div style={{ textAlign: `center` }}>
+      <br />
+      <h1>Page not found</h1>
+      <br/>
+      <h1>Opps! The page you are currently looking for is not available.</h1>
+    </div>
+  )
+}
